ec6bf352ef3581738d187eec18fbe8c1e936cd74
Ong Xuan Hong <ongxuanhong@admins-MacBook-Pro-5.local>
2016-03-16 14:04:11 +0700

    Complete wordcount by mongo

--- mongo_wordcount.py
+++ mongo_wordcount.py
@@ -1,17 +1,18 @@
 import sys, json, pymongo
 from pymongo import MongoClient
 from os.path import basename, splitext
+from bson.code import Code
 
 # Read data and store to mongoDB. Database: python_mapreduce, Collection: books
 def read_store_collection(filename):
 	# Open file and get collection name by file name
 	data = open(filename)
-	collection_name = splitext(filename)[0]
+	collection_name = splitext(basename(filename))[0]
 
 	# Connect to python_mapreduce database
 	client = MongoClient()
 	db = client.python_mapreduce
-	collection = db.collection_name
+	collection = db[collection_name]
 
 	# Check if collection books exists
 	if collection:
@@ -26,16 +27,47 @@ def read_store_collection(filename):
 		}
 
 		document_id = collection.insert(document)
-		print document_id
+		print "Insert document:", document_id
 
-# Read data from python_mapreduce/books
+def map_reduce_countword(filename, coll_out):
+	# get collection name by file name
+	collection_name = splitext(basename(filename))[0]
 
-# Define mapping function
+	# Connect to python_mapreduce database
+	client = MongoClient()
+	db = client.python_mapreduce
+	collection = db[collection_name]
 
-# Define reducing function
+	# Read data from python_mapreduce/books
+	books = collection.find()
+
+	# Define mapping function
+	map_func = Code(
+        '''
+        words = this.content.split(" ");
+        words.forEach(function(z) {
+        		emit(z, 1)
+        	}
+        );
+        '''
+    )
+
+	# Define reducing function
+	reduce_func = Code(
+		'''
+        function(key, values){
+        	return Array.sum(values)
+        }
+        '''
+	)
+
+	# Executing map-reduce
+	collection.map_reduce(map_func, reduce_func, coll_out)
+	print "Complete MapReduce"
 
 if __name__ == '__main__':
 	filename = sys.argv[1]
 	read_store_collection(filename)
 
 	# Execute mapreduce, store to Collection: books_wordcount
+	map_reduce_countword(filename, "books_wordcount")
